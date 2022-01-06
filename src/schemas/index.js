4cf4a6b9e48541bf95a014d6e2ba809e130ac991
Cali93 <armut.cumaali@gmail.com>
2022-01-06 10:15:06 +0100

    fix linter

--- src/schemas/index.js
+++
@@ -1,9 +0,0 @@
-const MemesSchema = require("./memes/memes-schema");
-const TagsSchema = require("./tags/tags-schema");
-
-module.exports = {
-  schemas: {
-    memes: MemesSchema,
-    tags: TagsSchema,
-  },
-};
